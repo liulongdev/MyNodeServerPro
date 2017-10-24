39facf180bac213ec869d8a4814a932b817e807d
liulongdev <305708612@qq.com>
2017-10-24 23:42:48 +0800

    优化代码

--- src/weixindev/index.js
+++ src/weixindev/index.js
@@ -12,10 +12,9 @@ const tuLingRobotAppKey = 'fe1a7ebad1e4d5ce85454b2c2f858a90';
 const xml2js = require('xml2js');
 const parseXMLString = xml2js.parseString;
 const builderXML = new xml2js.Builder({cdata:true});
-
+const async = require('async');
 
 function validateToken(req, res) {
-    console.log('>>>> validate token ...');
     const signature = req.query.signature;
     const timestamp = req.query.timestamp;
     const nonce = req.query.nonce;
@@ -39,7 +38,7 @@ function validateToken(req, res) {
     }
 }
 
-function getMessageFromGongZhongHao(req, res) {
+async function getMessageFromGongZhongHao(req, res) {
     let _da;
     req.on("data",function(data){
         /*微信服务器传过来的是xml格式的，是buffer类型，因为js本身只有字符串数据类型，所以需要通过toString把xml转换为字符串*/
@@ -47,79 +46,67 @@ function getMessageFromGongZhongHao(req, res) {
 
     });
     req.on("end",function(){
-        //console.log("end");
         parseXMLString(_da, function (err, result) {
-            console.log('>>>> result : >>>');
-            console.log(result);
             if (err)
             {
                 res.end('error');
                 return;
             }
+
             let isReturnError = false;
-            console.log('1. step >>>>>>>');
             if (result.xml && result.xml.MsgType)
             {
-                console.log('2. step >>>>>>>');
-                if (Array.isArray(result.xml.MsgType) && result.xml.MsgType.length > 0)
-                {
-                    console.log('3. step >>>>>>>');
-                    const msgType = result.xml.MsgType[0];
-                    const userId = result.xml.FromUserName[0];
-                    if (msgType == 'text')
-                    {
-                        console.log('4. step >>>>>>>');
-                        const content = result.xml.Content[0];
-                        request.post('http://www.tuling123.com/openapi/api')
-                            .send({key: tuLingRobotAppKey,
-                                info: content + '',
-                                userid: userId + ''})
-                            .end(function (err, response) {
-                                console.log('5. step >>>>>>>');
-                                console.log('tuling >>>>>>' + response);
-                                if (err)
-                                {
-                                    res.end('error');
-                                    return;
-                                }
-                                console.log('6. step >>>>>>>');
-                                let responseJson = JSON.parse(response.text);
-                                let replyContent = null;
-                                if (responseJson.code == 100000)
-                                {
-                                    console.log('7. step >>>>>>>');
-                                    replyContent = responseJson.text;
-                                    let temp = result.xml.FromUserName;
-                                    result.xml.ToUserName = result.xml.FromUserName;
-                                    result.xml.FromUserName = temp;
-                                    result.xml.Content = replyContent + '';
-
-                                    let xmlStr = builderXML.buildObject(result);
-                                    let xmlStrArray = xmlStr.split('?>');
-                                    let replyXML = (Array.isArray(xmlStrArray) && xmlStrArray.length > 1) ? xmlStrArray[1] : xmlStrArray[0];
-                                    console.log(replyXML);
-                                    res.end(replyXML);
-                                };
-                            });
-                    }
-                }
-                else
-                {
-                    isReturnError = true;
-                }
+                handleWeiXinMessage(res, result.xml);
             }
             else
             {
-                isReturnError = true;
-            }
-
-            if (isReturnError)
-            {
                 res.end('error');
             }
-
         });
     });
+}
+
+function handleWeiXinMessage(res, xmlJson) {
+        const msgType = xmlJson.MsgType[0];
+        const userId = xmlJson.FromUserName[0];
+        if (msgType == 'text')
+        {
+            const content = result.xml.Content[0];
+            request.post('http://www.tuling123.com/openapi/api')
+                .send({key: tuLingRobotAppKey,
+                    info: content + '',
+                    userid: userId + ''})
+                .end(function (err, response) {
+                    if (err)
+                    {
+                        res.end('error');
+                        return;
+                    }
+                    let responseJson = JSON.parse(response.text);
+                    let replyContent = null;
+                    // if (responseJson.code == 100000)
+                    // {
+                        replyContent = responseJson.text;
+                        let temp = result.xml.ToUserName;
+                        result.xml.ToUserName = result.xml.FromUserName;
+                        result.xml.FromUserName = temp;
+                        result.xml.Content = replyContent + '';
+
+                        let xmlStr = builderXML.buildObject(result);
+                        res.end(xmlStr);
+                    // }
+                });
+        }
+        else{
+            let temp = result.xml.ToUserName;
+            result.xml.ToUserName = result.xml.FromUserName;
+            result.xml.FromUserName = temp;
+            result.xml.Content = '请发文本哦';
+
+            let xmlStr = builderXML.buildObject(result);
+            res.end(xmlStr);
+        }
+
 }
 
 exports.validateToken = validateToken;
