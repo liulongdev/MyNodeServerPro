f4888ef328d8ea48194cf318a8c14f3118d4b525
liulongdev <305708612@qq.com>
2018-01-08 17:07:53 +0800

    增加一个委托调用接口

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ const wxSecret = '';
 const wxToken = 'Martin201710232344';
 const crypto = require('crypto');
 const _ = require('lodash');
+const MARResponseModel = require('./express/mar_response_model');
+const MARUtil = require('./lib/util');
 
 const express = require('express');
 const app = express();
@@ -39,10 +41,48 @@ app.get('/testRender', function (req, res, next) {
 const mar_express_api_core_validate = require('./express/api_validate');
 mar_express_api_core_validate(app);
 
+/*网易特殊url处理
+* url:
+* method:   POST GET
+* */
+app.post('/api/core/delegation/wy/special', function (req, res, next) {
+    let result = new MARResponseModel();
+    let params = req.body;
+    if (!MARUtil.verifyParams(params, ['url']))
+    {
+        result.header.errCode = 500;
+        result.header.errMsg = '参数错误';
+        res.end(result.toResponseJSON());
+        return;
+    }
+
+    if (params['method'] === 'POST')
+    {
+        res.json(result.toResponseJSON());
+    }
+    else
+    {
+        let url = params['url'];
+        request.get(url)
+            .end(function (err, response) {
+                if (err)
+                {
+                    result.header.errCode = 1;
+                    result.header.errMsg = '' + err;
+                }
+                else
+                {
+                    result.body = response.text;
+                    console.log('>>>>>>>> ' + response.text);
+                }
+                res.json(result.toResponseJSON());
+            });
+    }
+});
+
 /* 接口服务 */
 const mxr_express_server = require('./express/mar_express_server');
 mxr_express_server(app);
-
 
 app.all('/maxiaoding', function (req, res, next) {
     res.header('content-type', 'application/json;charset=utf-8');
@@ -103,7 +143,7 @@ function testQueryTrain() {
     console.log('>>>>> test ');
     let url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2017-12-18&leftTicketDTO.from_station=SZH&leftTicketDTO.to_station=SHH&purpose_codes=ADULT';
     let queryParams = {
-        'leftTicketDTO.train_date': '2018-01-04',
+        'leftTicketDTO.train_date': '2018-01-06',
         'leftTicketDTO.from_station': 'SZH',
         'leftTicketDTO.to_station': 'SHH',
         'purpose_codes': 'ADULT'
@@ -127,6 +167,40 @@ function testQueryTrain() {
             // console.log(res.body);
         });
 }
+
+
+function  testWangyiXin() {
+    let url = null;
+    // url = "https://c.m.163.com/recommend/getChanListNews?channel=T1456112438822&passport=&devId=4q9wyqO%2BqQBTBHM8f1fMsbVhBeT2jN%2BpF2piB1fvCVBW8vGHludHKO1HgHn4Q%2BQf&version=31.0&spever=false&net=wifi&lat=h4oCMn4567dmFOuemqUYsw%3D%3D&lon=thiEhYUT/OmIwZnpr9nr1A%3D%3D&ts=1515226754&sign=HdjOz40v0FJZW3%2B298s1AUZtrCVm%2BmHAuGmN8nwcfD948ErR02zJ6/KXOnxX046I&encryption=1&canal=appstore&offset=0&size=10&fn=1";
+    url = "https://c.m.163.com/recommend/getChanListNews?channel=T1457068979049&subtab=Video_Beauty&passport=&devId=I9mEvOjEpr7x6qKZ0E/jqz%2BXNYccr98jb6FUP/c34MDLszEoKIcbpbZWUwpshTwP&version=31.0&spever=false&net=wifi&lat=Db%2By8mQzKvnL76zse8k%2BFw%3D%3D&lon=YUz/CebUbKsPIsBavk9JUg%3D%3D&ts=1515393095&sign=7eFT5nh9qzmonv%2BiOvKmqmk6ziKtcFHuolPv9o5qzst48ErR02zJ6/KXOnxX046I&encryption=1&canal=appstore&offset=0&size=10&fn=2";
+    request.get(url)
+        // .set('Host', 'c.m.163.com')
+        // .set('Accept-Language', 'zh-Hans;q=1.0')
+        // .set('Accept', '*/*')
+        // .set('User-Agent', 'NewsApp/31.0 iOS/10.3.3 (iPhone5,2)')
+        // .set('User-L', '9GPoH3YTpi08QvHb/LQJH8wQis+pydvCwlKfLx+q3VlFLvuYyQq3vxyayVLVXEnh')
+        // .set('Accept-Encoding', 'gzip;q=1.0, compress;q=0.5')
+        // .set('Connection', 'keep-alive')
+        // .set('User-D', 'I9mEvOjEpr7x6qKZ0E/jqz+XNYccr98jb6FUP/c34MDLszEoKIcbpbZWUwpshTwP')
+        // .set('User-U', '')
+        // .set('X-Trace-Id', '1515380141_254548960_3361A073-8DA8-4133-AD36-027654329A12')
+        // // .set('User-C', '6KeG6aKROjrnvo7lpbM=')
+        // .set('User-N', 'yyXgmdmvoQtiN87jo3oYSdCd2giod6saU+xnB6AIucmi/Ns2KL3/3QwlaZ/+bual')
+        .end(function (err, res) {
+            if (err)
+            {
+                console.log(err);
+                reject(err +'');
+            }
+            else
+            {
+                console.log(res);
+                console.log(res.text);
+            }
+        });
+}
+
+// testWangyiXin();
 
 // setInterval(testQueryTrain, 1000);
 
